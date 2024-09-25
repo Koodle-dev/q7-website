19b72a90b6f4f9d41f7ebf72cf85bd1bbdc2f1ae
Leighton Lauton <103472406+fmp-llauton@users.noreply.github.com>
2024-09-25 16:01:30 +1000

    test

--- app/testimonials/page.js
+++ app/testimonials/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
